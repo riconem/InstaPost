9d2c4d49a0d8b3c047ddef50933ec62362f61dad
Enrico Nemitz <enrico.nemitz@telekom.de>
2019-05-27 23:15:20 +0200

    Update InstaPost.py

--- InstaPost.py
+++ InstaPost.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-#from PIL import Image
 import time
 import pyautogui
 
