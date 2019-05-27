5b9a2ad8bf2f1be96f560ab02662b0bc228a77c1
riconem <enrico_nemitz@hotmail.de>
2019-05-27 23:38:47 +0200

    remove bad interpreter

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ touch files/logs/uploaded.txt files/logs/uploaded_img.txt
 chmod +w files/logs/uploaded.txt files/logs/uploaded_img.txt
 
 cd assets
-
 #if this driver won't work you have to download a newer version.It depends on your chromium-browser or chrome.
 wget https://github.com/electron/electron/releases/download/v5.0.2/chromedriver-v5.0.2-linux-armv7l.zip
 unzip chromedriver-v5.0.2-linux-armv7l.zip
